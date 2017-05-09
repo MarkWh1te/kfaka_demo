1936b1f799c7dc6baaa50e09f65ce2da127fc3c2
markwh1te <iamwh1temark@outlook.com>
2017-05-08 20:28:55 -0700

    update

--- consumer.py
+++ consumer.py
@@ -14,16 +14,15 @@ def show_all_msg():
         if message is not None:
             print(message.offset, message.value)
 
-def show_partitions(offset):
-    # consumer = topic.get_simple_consumer(partitions)
-    consumer = topic.get_simple_consumer(
-    consumer_group="mygroup",
-    auto_offset_reset=OffsetType.LATEST
-)
-    for message in consumer:
-        if message is not None:
-            print(message.offset, message.value)
+# def show_partitions(offset):
+#     # consumer = topic.get_simple_consumer(partitions)
+#     consumer = topic.get_simple_consumer(
+#     consumer_group="mygroup",
+#     auto_offset_reset=OffsetType.LATEST
+# )
+#     for message in consumer:
+#         if message is not None:
+#             print(message.offset, message.value)
 
 if __name__ == '__main__':
     show_all_msg()
-    # show_partitions(400)
