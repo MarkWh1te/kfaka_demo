298b254df82f6ac8b30f1d9f70a8b8d804de6ec8
markwh1te <iamwh1temark@outlook.com>
2017-05-21 05:03:56 -0700

    fix bytes problem

--- consumer.py
+++ consumer.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
-from pykafka import KafkaClient
-from pykafka.common import OffsetType
+from kafka import KafkaConsumer
 
-client = KafkaClient('kfk0:11199')
-topic = client.topics['test9']
 
 def show_all_msg():
-    consumer = topic.get_simple_consumer(
-    # consumer_group="mygroup",
-    auto_offset_reset=OffsetType.LATEST
-    )
+    consumer = KafkaConsumer(bootstrap_servers='kfk0:11199')
+    consumer.subscribe(['test6','test9'])
     for message in consumer:
         if message is not None:
             print(message.offset, message.value)
-
-# def show_partitions(offset):
-#     # consumer = topic.get_simple_consumer(partitions)
-#     consumer = topic.get_simple_consumer(
-#     consumer_group="mygroup",
-#     auto_offset_reset=OffsetType.LATEST
-# )
-#     for message in consumer:
-#         if message is not None:
-#             print(message.offset, message.value)
 
 if __name__ == '__main__':
     show_all_msg()
--- producer.py
+++ producer.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
-from pykafka import KafkaClient
-
-client = KafkaClient('kfk0:11199')
-topic = client.topics['test9']
+from kafka import KafkaProducer
 
 def send_demo_msg(n):
-    with topic.get_producer() as producer:
-        counter = 0
-        while counter <= n:
-            producer.produce('this is message {} from mark'.format(counter), partition_key='{}'.format(counter))
-            counter += 1
-            # try:
-            #     msg, exc = producer.get_delivery_report(block=False)
-            #     if exc is not None:
-            #         print 'Failed to deliver msg {}: {}'.format(
-            #             msg.partition_key, repr(exc))
-            #     else:
-            #         print 'Successfully delivered msg {}'.format(
-            #             msg.partition_key)
-            # except Exception as e:
-            #     print(e)
-            #     break
+    producer = KafkaProducer(bootstrap_servers='kfk0:11199')
+    # for _ in range(n):
+    #     # producer.send('test9', b'some_message_bytes {}'.format(_))
+    #     producer.send('test9', bytes(a.format(_)))
+    #     # producer.send('test9', b'fff {}'.format(_))
+    #     print(_)
+
+    a = "fff {}"
+    future = producer.send('test9', bytes(a))
+    # future = producer.send('test9', b'another_message')
+    results = future.get(timeout=50)
+    print(results)
+
 
 if __name__ == '__main__':
-    send_demo_msg(500)
+    send_demo_msg(10)
